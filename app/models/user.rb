218b23829ecab6cccc1762310acf11be523af75b
patti8 <itupatti@gmail.com>
2023-04-11 11:02:18 +0900

    update commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         #:recoverable, 
+         :rememberable, 
+         :validatable
 
   after_create :add_stock
 
